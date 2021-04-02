2faea26339678a0c87e1af7db3d3fb686501c559
webdevstorage <46852738+webdevstorage@users.noreply.github.com>
2021-04-02 00:30:06 -0400

    update

--- app/mailer/admin.py
+++ app/mailer/admin.py
@@ -1,4 +1,18 @@
 #-*- coding: utf-8 -*-
 from django.contrib import admin
+from .models import Company, Contact, Order
 
-# Register your models here.
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ['name', 'bic']
+
+admin.site.register(Company, CompanyAdmin)
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ['company', 'first_name', 'last_name', 'email']
+
+admin.site.register(Contact, ContactAdmin)
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['order_number', 'company', 'contact', 'order_date']
+
+admin.site.register(Order, OrderAdmin)
